8debe390c32f1f7726d7153e7ce9f572d63a200d
renJava <rendg68@gmail.com>
2023-06-03 10:25:46 +0300

    слил историю в один коммит

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -8,18 +8,14 @@ import javax.persistence.Table;
 public class User {
     @Id
     private Long id;
-
     @Column
     private String name;
-
     @Column
     private String lastName;
-
     @Column
     private Byte age;
 
-    public User() {
-
+    public User() { //
     }
 
     public User(String name, String lastName, Byte age) {
@@ -59,4 +55,16 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                ", age = " + age +
+                '}';
+    }
+
+
 }
--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -4,6 +4,7 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
+
 public interface UserService {
     void createUsersTable();
 
